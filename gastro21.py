ccd0c78916cc8b997f8a2ef88feb61ae0e403f12
pjastr <24464854+pjastr@users.noreply.github.com>
2025-03-26 07:48:28 +0100

    add

--- gastro21.py
+++ gastro21.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
 data = pd.read_csv('gastro21.csv', header=None).T
-data.columns = ['Typ', 'Rok', 'Wartość']
-data['Rok'] = data['Rok'].astype(int)
-data['Wartość'] = data['Wartość'].astype(int)
+data.columns = ["Typ", "Rok", "Wartość"]
+data["Rok"] = data["Rok"].astype(int)
